b3639e8977afaaccdafa22c4c9f819519764b27e
Andreas Lagerkvist <powerbuoy@gmail.com>
2008-09-16 04:54:49 +0000

    Add copyright

---
+++ jquery.colourPicker.js
@@ -0,0 +1,11 @@
+/**
+ * @title:		Colour Picker
+ * @version:	2.0
+ * @author:		Andreas Lagerkvist
+ * @date:		2008-08-31
+ * @url:		http://andreaslagerkvist.com/jquery/colour-picker/
+ * @usage:		$('select[name="colour"]').colourPicker(); Turns a drop-down full of colours into a colour-picker dialogue
+ * @license:	http://creativecommons.org/licenses/by/3.0/
+ * @copyright:	2008 Andreas Lagerkvist (andreaslagerkvist.com)
+ * @requires:	jQuery, Dimensions
+ **/
