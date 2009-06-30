9b15f76dea9fa95120bb2524d81304d4586fc3db
Andreas Lagerkvist <powerbuoy@gmail.com>
2009-06-30 20:42:26 +0000

    New implementation

--- jquery.colourPicker.js
+++ jquery.colourPicker.js
@@ -80,6 +80,128 @@ Use it like this: <select name="colour"><?php gwsc(); ?></select>.
 @exampleJS:
 jQuery('#jquery-colour-picker-example select').colourPicker({ico: WEBROOT +'aFramework/Styles/__common/gfx/jquery.colourPicker.gif'});
 ***/
+jQuery.fn.colourPicker = function(conf) {
+	// Config for plug
+	var config = jQuery.extend({
+		ID:		'colour-picker',	// ID of colour-picker container
+		ico:		'ico.gif',		// SRC to colour-picker icon
+		title:		'Pick a colour',	// Default dialogue title
+		inputBG:	true,			// Whether to change the input's background to the selected colour's
+		speed:		500,			// Speed of dialogue-animation
+		allowTransparent:	false, 
+		transparentTxt:		'Transparent', 
+		transparentVal:		''
+	}, conf);
+
+	// Inverts a hex-colour
+	var hexInvert = function(hex) {
+		var r = hex.substr(0, 2);
+		var g = hex.substr(2, 2);
+		var b = hex.substr(4, 2);
+
+		return 0.212671 * r + 0.715160 * g + 0.072169 * b < 0.5 ? 'ffffff' : '000000'
+	};
+
+	// Add the colour-picker dialogue if not added
+	var colourPicker = jQuery('#' +config.ID);
+
+	if(!colourPicker.length) {
+		colourPicker = jQuery('<div id="' +config.ID +'"></div>').appendTo(document.body).hide();
+
+		// Remove the colour-picker if you click outside it (on body)
+		jQuery(document.body).click(function(event) {
+			if(!(jQuery(event.target).is(':input') || jQuery(event.target).is('#' +config.ID) || jQuery(event.target).parents('#' +config.ID).length)) {
+				colourPicker.hide(config.speed);
+			}
+		});
+	}
+
+	// For every select passed to the plug-in
+	return this.each(function() {
+		// Insert icon and input
+		var select	= jQuery(this);
+		var icon	= jQuery('<a href="#"><img src="' +config.ico +'" alt="Open colour picker" /></a>').insertAfter(select);
+		var input	= jQuery('<input type="text" name="' +select.attr('name') +'" value="' +select.val() +'" size="6" />').insertAfter(select);
+		var loc		= '';
+
+		// Build a list of colours based on the colours in the select
+		jQuery('option', select).each(function() {
+			var option	= jQuery(this);
+			var hex		= option.val();
+			var title	= option.text();
+
+			loc += '<li><a href="#" title="' +title +'" rel="' +hex +'" style="background: #' +hex +'; colour: ' +hexInvert(hex) +';">' +title +'</a></li>';
+		});
+
+		// Remove select
+		select.remove();
+
+		// If user wants to, change the input's BG to reflect the newly selected colour
+		if(config.inputBG) {
+			input.change(function() {
+				var bg = '#' +input.val();
+
+				if (bg == '#' + config.transparentVal) {
+					var bg = 'transparent';
+				}
+
+				input.css({
+					background: bg, 
+					color: '#' +hexInvert(input.val())
+				});
+			});
+		}
+
+		// When you click the icon
+		icon.click(function() {
+			openColourPicker();
+
+			return false;
+		});
+
+		// Or focus the input
+		input.focus(function() {
+			openColourPicker();
+
+			return false;
+		});
+
+		// Open the colour picker
+		var openColourPicker = function() {
+			// Show the colour-picker next to the icon and fill it with the colours in the select that used to be there
+			var iconPos	= icon.offset();
+			var heading	= config.title ? '<h2>' +config.title +'</h2>' : '';
+			var trans	= config.allowTransparent ? '<a href="#" rel="' +config.transparentVal +'">' +config.transparentTxt +'</a>' : '';
+
+			colourPicker
+				.html(heading +'<ul>' +loc +'</ul>' +trans)
+				.css({
+					position: 'absolute', 
+					left: iconPos.left +'px', 
+					top: iconPos.top +'px'
+				})
+				.show(config.speed, function () {
+					if (jQuery.fn.bgiframe) {
+						colourPicker.bgiframe();
+					}
+				});
+
+			// When you click a colour in the colour-picker
+			jQuery('a', colourPicker).click(function() {
+				// The hex is stored in the link's rel-attribute
+				var hex = jQuery(this).attr('rel');
+
+				// Change val and trigger change-event on input
+				input.val(hex).change();
+
+				// Hide the colour-picker and return false
+				colourPicker.hide(config.speed);
+
+				return false;
+			});
+		};
+	});
+};
 jQuery.fn.colourPicker = function (conf) {
 	// Config for plug
 	var config = jQuery.extend({
